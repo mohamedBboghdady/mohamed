92ef92a1fe7c91b4f270a6d8f82b9d32e20b8f64
unknown <mohamedbogdady@gmail.com>
2024-02-12 16:25:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+//  var button=document.getElementById("submit")
+//  button.addEventListener("click",login)
+//  var Data_fullname="mohamed"j
+// var Data_paswoord="12345678"
+// var Data_confermpassword="12345678"
+//      function login(e) {}
+//       e.preventDefault()
+//       var inputname=document.getElementById("name").value
+//       var inputpaswoord=document.getElementById("password").value
+//       var inputconfrempassword=document.getElementById("confermpassword").value
+//       if(inputname==Data_fullname&&inputpaswoord==Data_paswoord&& inputconfrempassword==Data_confermpassword){
+//          alert("HELLO"   + Data_fullname)
+//          console.log(true)
+//       }
+//       else{
+//          alert("pleas tray agen")
+//          console.log(false)
+
+      
+//       }
+//  var button=document.getElementById("signup")
+//  button.addEventListener("signup", signup)
+// function signup(e){}
+//    e.preventDefault();
+
+//    var inputs=document.querySelectorAll(".inputs")
+//    for (let i = 0; i < inputs.length; i++) {
+//       var input = inputs[i];
+//       if(input.value==""){
+
+//          input.Placeholder="empty"
+      
+      
+//    }
+   
+// }
+    document.querySelector(".navbar .list")
+    .addEventListener("click" ,function () {
+     let show=document.querySelector(".icon_vav")
+     show.classList.toggle("posh")
+     
+    }
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+// setTimeout
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
